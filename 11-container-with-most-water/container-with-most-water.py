87122aebec110b3d326395f4219dcd24a75ccec1
Nababrato2003 <158729490+Nababrato2003@users.noreply.github.com>
2025-06-05 22:53:45 +0530

    Update container-with-most-water.py

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def maxArea(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
+      
         max_area = 0
         left = 0
         right = len(height)-1
