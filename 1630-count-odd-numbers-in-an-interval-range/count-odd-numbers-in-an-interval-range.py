01f1c5fa860150080c2d3f8bc30930a5dbb9e4e5
Nababrato2003 <158729490+Nababrato2003@users.noreply.github.com>
2025-06-08 13:17:45 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (97.44%) - LeetSync

--- 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.py
+++ 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.py
@@ -2,3 +2,11 @@ class Solution(object):
     def countOdds(self, low, high):
        a = (high+1)//2 - (low//2)
        return a
+
+       '''(high + 1) // 2 - (low // 2)
+       Counts how many odd numbers are in the range [low, high].'''
+
+       
+       '''(7 + 1) // 2 - (3 // 2)
+        = 8 // 2 - 1
+        = 4 - 1 = 3 ✅'''
