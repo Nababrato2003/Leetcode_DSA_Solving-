47e79e85b84246628cb97b791cae3bed367fc3ca
Nababrato2003 <158729490+Nababrato2003@users.noreply.github.com>
2025-06-08 12:51:36 +0530

    Time: 1 ms (62.75%) | Memory: 13.3 MB (59.35%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -1,6 +1,8 @@
 class Solution(object):
     def fizzBuzz(self, n):
-        ans = []
+        ans = [] #ans will store all the answers ("Fizz", "Buzz", numbers, etc.)
+
+
         for i in range(1,n+1):
             if i%3==0 and i%5==0:
                 ans.append("FizzBuzz")
@@ -13,4 +15,12 @@ class Solution(object):
 
         return ans
         
+    '''| i | Condition     | What is appended | ans list                          |
+| - | ------------- | ---------------- | --------------------------------- |
+| 1 | not divisible | `"1"`            | `["1"]`                           |
+| 2 | not divisible | `"2"`            | `["1", "2"]`                      |
+| 3 | i % 3 == 0    | `"Fizz"`         | `["1", "2", "Fizz"]`              |
+| 4 | not divisible | `"4"`            | `["1", "2", "Fizz", "4"]`         |
+| 5 | i % 5 == 0    | `"Buzz"`         | `["1", "2", "Fizz", "4", "Buzz"]` |
+'''
         
