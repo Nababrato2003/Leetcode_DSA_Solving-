84bbbe57513ef2277e46db004560336e1eee0967
Nababrato2003 <158729490+Nababrato2003@users.noreply.github.com>
2025-06-12 20:29:03 +0530

    Time: 4 ms (42.11%) | Memory: 12.5 MB (42.11%) - LeetSync

--- 3747-maximum-difference-between-adjacent-elements-in-a-circular-array/maximum-difference-between-adjacent-elements-in-a-circular-array.py
+++ 3747-maximum-difference-between-adjacent-elements-in-a-circular-array/maximum-difference-between-adjacent-elements-in-a-circular-array.py
@@ -1,16 +1,29 @@
-class Solution(object):
-    def maxAdjacentDistance(self, nums):
-      n = len(nums)
-      res =  float("-inf")
+class Solution(object): # Define a class named Solution
+    def maxAdjacentDistance(self, nums):# Define a method inside the class that takes a list `nums`
+      n = len(nums)# Store the length of the list in variable `n`
+      res =  float("-inf")# Initialize result with negative infinity
         # print(res)
-      for i in range(1, n):
-        if i == n-1:
-                diff = abs(nums[-1] - nums[0])
-                res = max(res, diff)
+      for i in range(1, n): # Loop from index 1 to n-1
+        if i == n-1: # If we are at the last element of the list
+                diff = abs(nums[-1] - nums[0])# Difference between last and first element
+                res = max(res, diff) # Update the result if this difference is larger
                 # print("last: ",diff)
-        diff = abs(nums[i-1] - nums[i])
+        diff = abs(nums[i-1] - nums[i])# Difference between current and previous elemen
                 # print("loop: ",diff)
-        res = max(res, diff)
+        res = max(res, diff) # Update max result if this difference is larger
         
         # print(res)
-      return res
+      return res# Finally, return the largest adjacent difference found
+
+      '''If nums = [5, 2, 9, 4]:
+
+Differences:
+|5 - 2| = 3
+|2 - 9| = 7
+|9 - 4| = 5
+|4 - 5| = 1 ← last and first
+
+Maximum = 7
+
+'''
+
